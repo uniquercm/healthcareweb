6169baf9e62b755e7158d5d5241d0c64c9cc6b91
uniquercm <84480377+uniquercm@users.noreply.github.com>
2021-06-07 00:59:27 +0530

    changes

--- src/app/postlogin/list/list.component.ts
+++ src/app/postlogin/list/list.component.ts
@@ -15,7 +15,7 @@ export class ListComponent implements OnInit {
 
   array = [];
   displayedColumns: string[] = ['id', 'requestCrmName', 'crmNo', 'patientName', 'eidNo', 'mobileNo', 'edit', 'reception', 'schedule', 'drcall', 'nursecall'];
-  dataSource: any = new MatTableDataSource(array);
+  dataSource: any = new MatTableDataSource([]);
 
   @ViewChild(MatPaginator) paginator: MatPaginator = new MatPaginator(new MatPaginatorIntl(), ChangeDetectorRef.prototype);
   @ViewChild(MatSort) sort: MatSort = new MatSort();
@@ -26,13 +26,14 @@ export class ListComponent implements OnInit {
   todate: any;
 
   requestarray: any[] = [];
+  area: any[] = [];
 
   constructor(private commonService: CommonService, public datepipe: DatePipe) {
     if (this.localvalues.userType === 6) {
       this.displayedColumns = ['id', 'requestCrmName', 'crmNo', 'patientName', 'eidNo', 'mobileNo', 'edit', 'reception', 'print'];
     } else if (this.localvalues.userType === 1) {
     }
-
+    this.getarea();
     this.getreq();
     this.getPatent('');
   }
@@ -59,13 +60,33 @@ export class ListComponent implements OnInit {
     })
   }
 
-  select(event: any) {
+  select(name: string, event: any) {
     let farray: any = [];
-    this.array.forEach((element: any) => {
-      if (element.requestId === Number(event.value)) {
-        farray.push(element);
-      }
-    });
+    if (name === 'case') {
+      this.array.forEach((element: any) => {
+        if (element.patientInformation.requestId === Number(event.value)) {
+          farray.push(element);
+        }
+      });
+    } else if (name === 'area') {
+      this.array.forEach((element: any) => {
+        if (element.patientInformation.areaId === Number(event.value)) {
+          farray.push(element);
+        }
+      });
+    } else {
+      this.array.forEach((element: any) => {
+        if (event.value === 'yes') {
+          if (element.googleMapLink !== '')
+            farray.push(element);
+        } else if (event.value === 'no') {
+          if (element.googleMapLink === '')
+            farray.push(element);
+        } else {
+          farray.push(element);
+        }
+      });
+    }
 
     this.dataSource = new MatTableDataSource(farray);
 
@@ -112,50 +133,15 @@ export class ListComponent implements OnInit {
     }, err => {
       console.log(err);
     })
-
   }
+
+  getarea() {
+    this.commonService.getmethod('area').subscribe((data) => {
+      this.area = data.details;
+    }, err => {
+      console.log(err);
+    })
+  }
+
 
 }
-
-const array = [
-  {
-    no: 1,
-    crmtype: 'HQP',
-    crmno: '1234',
-    name: 'Mohmamed',
-    eid: '111-1111-1111111-6',
-    mobile: '+971 55 378 9865'
-  },
-  {
-    no: 2,
-    crmtype: 'HIP',
-    crmno: '4589',
-    name: 'Anitha',
-    eid: '111-1111-1111111-6',
-    mobile: '+971 55 378 9865'
-  },
-  {
-    no: 3,
-    crmtype: 'HIP',
-    crmno: '5789',
-    name: 'Kesavan',
-    eid: '111-1111-1111111-6',
-    mobile: '+971 55 378 9865'
-  },
-  {
-    no: 4,
-    crmtype: 'HQP',
-    crmno: '8699',
-    name: 'Alina',
-    eid: '111-1111-1111111-6',
-    mobile: '+971 55 378 9865'
-  },
-  {
-    no: 5,
-    crmtype: 'CRM',
-    crmno: '2019',
-    name: 'Ramya',
-    eid: '111-1111-1111111-6',
-    mobile: '+971 55 378 9865'
-  }
-]
