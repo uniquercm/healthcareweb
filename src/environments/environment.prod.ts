afcf42ae85226f481db8fb62ed82a3b5a43beb5f
uniquercm <84480377+uniquercm@users.noreply.github.com>
2021-08-21 00:38:27 +0530

    Change the Url Referene

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   //Thanam 18-08-21
-  url: 'http://demoapi.uniquercm.org/v1/'
-  //url: 'http://api.uniquercm.org/v1/'
+  //url: 'http://demoapi.uniquercm.org/v1/'
+  url: 'http://api.uniquercm.org/v1/'
   //************ */
 };
